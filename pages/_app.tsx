369e455845021fb14c54adad32747cea7ecb262e
asiimawearnold <asiimwearnold25@gmail.com>
2022-06-15 11:16:16 +0300

    Fixed Error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import type { AppProps } from 'next/app'
 import { MoralisProvider } from 'react-moralis'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <MoralisProvider  appId = {process.env.NEXT_PUBLIC_APP_ID} serverUrl={process.env.NEXT_PUBLIC_SERVER}>
+  return <MoralisProvider  appId = {process.env.NEXT_PUBLIC_APP_ID ?? "AppId Undefined Fallback"} serverUrl={process.env.NEXT_PUBLIC_SERVER ?? "ServerUrl Undefined Fallback"}>
             <Component {...pageProps} />
           </MoralisProvider>
   
