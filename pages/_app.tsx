20051d1a1f7999d340486fbb81d7121a09f27ff1
asiimawearnold <asiimwearnold25@gmail.com>
2022-06-15 14:03:13 +0300

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { MoralisProvider } from 'react-moralis'
 
+
 function MyApp({ Component, pageProps }: AppProps) {
   return <MoralisProvider  appId = {process.env.NEXT_PUBLIC_APP_ID ?? "AppId Undefined Fallback"} serverUrl={process.env.NEXT_PUBLIC_SERVER ?? "ServerUrl Undefined Fallback"}>
             <Component {...pageProps} />
